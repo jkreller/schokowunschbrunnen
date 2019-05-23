7540edd3b0eb6ec7e61dbb64902fb79575615c9c
Julian Kreller <jkreller@eos-uptrade.de>
2019-05-23 18:29:19 +0200

    * Edit shop product page

--- routes/index.js
+++ routes/index.js
@@ -1,35 +1,33 @@
 const express = require('express');
 const router = express.Router();
-var Chocolate = require('../models/Chocolate');
+const Chocolate = require('../models/Chocolate');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index');
 });
 
 /* GET choco wishing well. */
-router.get('/wishing-well', function(req, res, next) {
+router.get('/wishing-well', function (req, res, next) {
   res.render('schoko-wunschbrunnen');
 });
 
 /* GET choco shop. */
-router.get('/shop', function(req, res, next) {
-  Chocolate.find({"selfmade":false}).then(function(chocolates){
-      res.render('schoko-shop', {chocolates: chocolates});
+router.get('/shop', function (req, res, next) {
+  Chocolate.find({"selfmade": false}).then(function (chocolates) {
+    res.render('schoko-shop', {chocolates: chocolates});
   });
-
 });
 
-
-router.get('/shop/:productId', function(req, res, next) {
-  Chocolate.find({"_id":req.params.productId}).then(function(chocolate){
-      console.log(chocolate);
-      res.render('produktseite', {chocolate: chocolate});
+/* GET choco shop product. */
+router.get('/shop/:productId', function (req, res, next) {
+  Chocolate.findOne({'_id': req.params.productId}).then(function (chocolate) {
+    res.render('produktseite', {chocolate: chocolate});
   });
 });
 
 /* GET shopping cart. */
-router.get('/shopping-cart', function(req, res, next) {
+router.get('/shopping-cart', function (req, res, next) {
   res.render('warenkorb');
 });
 
--- routes/user.js
+++ routes/user.js
@@ -8,90 +8,85 @@ const bcrypt = require('bcryptjs');
 
 /* GET register. */
 router.get('/register', function (req, res, next) {
-    res.render('user/registrieren', {message: req.flash('error')});
+  res.render('user/registrieren', {message: req.flash('error')});
 });
 
 /* POST register. */
 router.post('/register', passport.authenticate('local.signup', {
-    successRedirect: '/',
-    failureRedirect: '/user/register',
-    badRequestMessage: 'Da fehlt aber noch was...!?',
-    failureFlash: true
+  successRedirect: '/',
+  failureRedirect: '/user/register',
+  badRequestMessage: 'Da fehlt aber noch was...!?',
+  failureFlash: true
 }));
 
 /* GET login. */
 router.get('/login', function (req, res, next) {
-    res.render('user/anmelden', {message: req.flash('error')});
+  res.render('user/anmelden', {message: req.flash('error')});
 });
 
 /* POST login. */
 router.post('/login', passport.authenticate('local.signin', {
-    successRedirect: '/',
-    failureRedirect: '/user/login',
-    badRequestMessage: 'Da fehlt aber noch was...!?',
-    failureFlash: true
+  successRedirect: '/',
+  failureRedirect: '/user/login',
+  badRequestMessage: 'Da fehlt aber noch was...!?',
+  failureFlash: true
 }));
 
 router.get('/logout', function (req, res, next) {
-    req.logout();
-    res.redirect('/');
+  req.logout();
+  res.redirect('/');
 });
 
 /* GET profile. */
 router.get('/profile', loginHandler.ensureAuthentication, function (req, res, next) {
-    const birthdayFormatted = moment().format('DD.MM.YYYY');
-    res.render('user/profil', {user: req.user, birthdayFormatted: birthdayFormatted});
+  const birthdayFormatted = moment().format('DD.MM.YYYY');
+  res.render('user/profil', {user: req.user, birthdayFormatted: birthdayFormatted});
 });
 
 /* GET profile edit. */
 router.get('/profile/edit', loginHandler.ensureAuthentication, function (req, res, next) {
-    const birthdayFormatted = moment().format('YYYY-MM-DD');
-    res.render('user/profil', {user: req.user, birthdayFormatted: birthdayFormatted, edit: true});
+  const birthdayFormatted = moment().format('YYYY-MM-DD');
+  res.render('user/profil', {user: req.user, birthdayFormatted: birthdayFormatted, edit: true});
 });
 
 /* POST profile edit. */
 router.post('/profile/edit', loginHandler.ensureAuthentication, function (req, res, next) {
-    res.render('user/profil', {user: req.user, edit: false});
+  res.render('user/profil', {user: req.user, edit: false});
 });
 
 router.get('/profile/edit-password', loginHandler.ensureAuthentication, function (req, res, next) {
-    res.render('user/change-password', {user: req.user});
+  res.render('user/change-password', {user: req.user});
 });
 
 router.post('/profile/edit-password', loginHandler.ensureAuthentication, async function (req, res, next) {
-    console.log("es geht los");
-    let oldPasswordMatch;
-    try {
-        oldPasswordMatch = await bcrypt.compare(req.body.oldpassword, req.user.password);
-    } catch (e) {
-        next(e);
-    }
+  let oldPasswordMatch;
+  try {
+    oldPasswordMatch = await bcrypt.compare(req.body.oldpassword, req.user.password);
+  } catch (e) {
+    next(e);
+  }
 
-    if (oldPasswordMatch) {
-        if (req.body.password === req.body.passwordrepeat) {
-
-            bcrypt.hash(req.body.password, 10, function (err, hash2) {
-                if (err) {
-                    next(err);
-                }
-                const newUser = {
-                    "password": hash2
-                };
-                User.findByIdAndUpdate(req.user._id, newUser).then(function (err) {
-
-                    res.render('user/profil');
-
-                });
-
-            });
-
-        } else {
-            res.render('user/change-password', {message: 'Passwörter stimmen nicht überein!'});
+  if (oldPasswordMatch) {
+    if (req.body.password === req.body.passwordrepeat) {
+      bcrypt.hash(req.body.password, 10, function (err, hash2) {
+        if (err) {
+          next(err);
         }
-    } else {
-        res.render('user/change-password', {message: 'Falsches Passwort!'});
 
+        const newUser = {
+          "password": hash2
+        };
+
+        User.findByIdAndUpdate(req.user._id, newUser).then(function (err) {
+          res.render('user/profil');
+        });
+      });
+    } else {
+      res.render('user/change-password', {message: 'Passwörter stimmen nicht überein!'});
     }
+  } else {
+    res.render('user/change-password', {message: 'Falsches Passwort!'});
+  }
 });
 
 module.exports = router;
