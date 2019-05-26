5f775c92cc6f8ce6dffc21c6ffd26f07d219c03d
Julian Kreller <jkreller@eos-uptrade.de>
2019-05-26 22:31:59 +0200

    * Add missing renamed models

---
+++ models/Cream.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const schema = new Schema({
+    name: {type: String, required: true},
+    price: {type: Number, required: true},
+    className: {type: String, required: true}
+});
+
+module.exports = mongoose.model('Cream', schema);
