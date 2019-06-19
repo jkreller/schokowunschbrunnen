c926072b789c24915c7ea571de1e81b1a269afec
Verena L <verena.lehmann.98@gmail.com>
2019-06-19 16:52:26 +0200

    price-calculation at Schokowunschbrunnen

--- public/javascript/wishing-well.js
+++ public/javascript/wishing-well.js
@@ -23,6 +23,19 @@ var imgtopping = document.getElementById("topping");
 var imgsideview = document.getElementById("side-view");
 var imgtopview = document.getElementById("top-view");
 
+var totalPriceField = document.getElementById("totalprice");
+var creamPriceField = document.getElementById("creamprice");
+var stuffingPriceField = document.getElementById("stuffingprice");
+var toppingPriceField = document.getElementById("toppingprice");
+
+console.log(creamPriceField.innerText);
+
+var totalPrice = 1.00;
+var creamPrice = 0.00;
+var stuffingPrice = 0.00;
+var toppingPrice = 0.00;
+
+// the function changeShape 
 function changeShape(event) {
     if ( this.id === 'rectangle' ) {
         for(i=0; i<3; i++){
@@ -53,6 +66,7 @@ function changeShape(event) {
 Array.prototype.forEach.call(shaperadios, function(radio) {
     radio.addEventListener('change', changeShape);
 });
+
 function changeVariety(event) {
     if ( this.id === 'dark' ) {
         for(j=0; j<3; j++){
@@ -87,87 +101,105 @@ Array.prototype.forEach.call(varietyradios, function(radio) {
 
 /*Creme-Bild wechseln*/
 function changeCream(event) {
-if (this.id === 'no-cream') {
-  imgcream.classList.remove('cream1', 'cream2', 'cream3');
-  imgcream.classList.add('cream0');
-  ZusammenfassungCremeFüllung[0].classList.remove('cream-1', 'cream-2', 'cream-3');
-  ZusammenfassungCremeFüllung[0].classList.add('cream-0');
-} else if (this.id === 'pistachio') {
-  imgcream.classList.remove('cream0', 'cream2', 'cream3');
-  imgcream.classList.add('cream1');
-  ZusammenfassungCremeFüllung[0].classList.remove('cream-0', 'cream-2', 'cream-3');
-  ZusammenfassungCremeFüllung[0].classList.add('cream-1');
-} else if (this.id === 'vanilla') {
-  imgcream.classList.remove('cream0', 'cream1', 'cream3');
-  imgcream.classList.add('cream2');
-  ZusammenfassungCremeFüllung[0].classList.remove('cream-0', 'cream-1', 'cream-3');
-  ZusammenfassungCremeFüllung[0].classList.add('cream-2');
-} else if (this.id === 'raspberry') {
-  imgcream.classList.remove('cream0', 'cream1', 'cream2');
-  imgcream.classList.add('cream3');
-  ZusammenfassungCremeFüllung[0].classList.remove('cream-0', 'cream-1', 'cream-2');
-  ZusammenfassungCremeFüllung[0].classList.add('cream-3');
+    if (this.id === 'no-cream') {
+    imgcream.classList.remove('cream1', 'cream2', 'cream3');
+    imgcream.classList.add('cream0');
+    ZusammenfassungCremeFüllung[0].classList.remove('cream-1', 'cream-2', 'cream-3');
+    ZusammenfassungCremeFüllung[0].classList.add('cream-0');
+    creamPrice = 0.00;
+    } else if (this.id === 'pistachio') {
+    imgcream.classList.remove('cream0', 'cream2', 'cream3');
+    imgcream.classList.add('cream1');
+    ZusammenfassungCremeFüllung[0].classList.remove('cream-0', 'cream-2', 'cream-3');
+    ZusammenfassungCremeFüllung[0].classList.add('cream-1');
+    creamPrice = 0.65;
+    } else if (this.id === 'vanilla') {
+    imgcream.classList.remove('cream0', 'cream1', 'cream3');
+    imgcream.classList.add('cream2');
+    ZusammenfassungCremeFüllung[0].classList.remove('cream-0', 'cream-1', 'cream-3');
+    ZusammenfassungCremeFüllung[0].classList.add('cream-2');
+    creamPrice = 0.45;
+    } else if (this.id === 'raspberry') {
+    imgcream.classList.remove('cream0', 'cream1', 'cream2');
+    imgcream.classList.add('cream3');
+    ZusammenfassungCremeFüllung[0].classList.remove('cream-0', 'cream-1', 'cream-2');
+    ZusammenfassungCremeFüllung[0].classList.add('cream-3');
+    creamPrice = 0.55;
+    }
+    creamPriceField.innerText = creamPrice.toFixed(2).replace('.',',');
 }
-}
-
 Array.prototype.forEach.call(creamradios, function (radio) {
 radio.addEventListener('change', changeCream);
 });
 
 /*Füllung-Bild wechseln*/
 function changeStuffing(event) {
-if (this.id === 'no-stuffing') {
-  imgstuffing.classList.remove('stuffing1', 'stuffing2', 'stuffing3');
-  imgstuffing.classList.add('stuffing0');
-  ZusammenfassungCremeFüllung[1].classList.remove('stuffing-1', 'stuffing-2', 'stuffing-3');
-  ZusammenfassungCremeFüllung[1].classList.add('stuffing-0');
-} else if (this.id === 'blueberry') {
-  imgstuffing.classList.remove('stuffing0', 'stuffing2', 'stuffing3');
-  imgstuffing.classList.add('stuffing1');
-  ZusammenfassungCremeFüllung[1].classList.remove('stuffing-0', 'stuffing-2', 'stuffing-3');
-  ZusammenfassungCremeFüllung[1].classList.add('stuffing-1');
-} else if (this.id === 'hazelnut') {
-  imgstuffing.classList.remove('stuffing0', 'stuffing1', 'stuffing3');
-  imgstuffing.classList.add('stuffing2');
-  ZusammenfassungCremeFüllung[1].classList.remove('stuffing-0', 'stuffing-1', 'stuffing-3');
-  ZusammenfassungCremeFüllung[1].classList.add('stuffing-2');
-} else if (this.id === 'oreo') {
-  imgstuffing.classList.remove('stuffing0', 'stuffing1', 'stuffing2');
-  imgstuffing.classList.add('stuffing3');
-  ZusammenfassungCremeFüllung[1].classList.remove('stuffing-0', 'stuffing-1', 'stuffing-2');
-  ZusammenfassungCremeFüllung[1].classList.add('stuffing-3');
+    if (this.id === 'no-stuffing') {
+        imgstuffing.classList.remove('stuffing1', 'stuffing2', 'stuffing3');
+        imgstuffing.classList.add('stuffing0');
+        ZusammenfassungCremeFüllung[1].classList.remove('stuffing-1', 'stuffing-2', 'stuffing-3');
+        ZusammenfassungCremeFüllung[1].classList.add('stuffing-0');
+        stuffingPrice = 0.00;
+    } else if (this.id === 'blueberry') {
+        imgstuffing.classList.remove('stuffing0', 'stuffing2', 'stuffing3');
+        imgstuffing.classList.add('stuffing1');
+        ZusammenfassungCremeFüllung[1].classList.remove('stuffing-0', 'stuffing-2', 'stuffing-3');
+        ZusammenfassungCremeFüllung[1].classList.add('stuffing-1');
+        stuffingPrice = 0.55;
+    } else if (this.id === 'hazelnut') {
+        imgstuffing.classList.remove('stuffing0', 'stuffing1', 'stuffing3');
+        imgstuffing.classList.add('stuffing2');
+        ZusammenfassungCremeFüllung[1].classList.remove('stuffing-0', 'stuffing-1', 'stuffing-3');
+        ZusammenfassungCremeFüllung[1].classList.add('stuffing-2');
+        stuffingPrice = 0.45;
+    } else if (this.id === 'oreo') {
+        imgstuffing.classList.remove('stuffing0', 'stuffing1', 'stuffing2');
+        imgstuffing.classList.add('stuffing3');
+        ZusammenfassungCremeFüllung[1].classList.remove('stuffing-0', 'stuffing-1', 'stuffing-2');
+        ZusammenfassungCremeFüllung[1].classList.add('stuffing-3');
+        stuffingPrice = 0.65;
+    }
+    stuffingPriceField.innerText = stuffingPrice.toFixed(2).replace('.',',');
 }
-}
-
 Array.prototype.forEach.call(stuffingradios, function (radio) {
 radio.addEventListener('change', changeStuffing);
 });
 
 /*Topping-Bild wechseln*/
 function changeTopping(event) {
-if (this.id === 'no-topping') {
-  imgtopping.classList.remove('topping1', 'topping2', 'topping3');
-  imgtopping.classList.add('topping0');
-  ZusammenfassungTopping[0].classList.remove('topping-1', 'topping-2', 'topping-3');
-  ZusammenfassungTopping[0].classList.add('topping-0');
-} else if (this.id === 'strawberry') {
-  imgtopping.classList.remove('topping0', 'topping2', 'topping3');
-  imgtopping.classList.add('topping1');
-  ZusammenfassungTopping[0].classList.remove('topping-0', 'topping-2', 'topping-3');
-  ZusammenfassungTopping[0].classList.add('topping-1');
-} else if (this.id === 'smarties') {
-  imgtopping.classList.remove('topping0', 'topping1', 'topping3');
-  imgtopping.classList.add('topping2');
-  ZusammenfassungTopping[0].classList.remove('topping-0', 'topping-1', 'topping-3');
-  ZusammenfassungTopping[0].classList.add('topping-2');
-} else if (this.id === 'gold') {
-  imgtopping.classList.remove('topping0', 'topping1', 'topping2');
-  imgtopping.classList.add('topping3');
-  ZusammenfassungTopping[0].classList.remove('topping-0', 'topping-1', 'topping-2');
-  ZusammenfassungTopping[0].classList.add('topping-3');
+    if (this.id === 'no-topping') {
+        imgtopping.classList.remove('topping1', 'topping2', 'topping3');
+        imgtopping.classList.add('topping0');
+        ZusammenfassungTopping[0].classList.remove('topping-1', 'topping-2', 'topping-3');
+        ZusammenfassungTopping[0].classList.add('topping-0');
+        toppingPrice = 0.00;
+    } else if (this.id === 'strawberry') {
+        imgtopping.classList.remove('topping0', 'topping2', 'topping3');
+        imgtopping.classList.add('topping1');
+        ZusammenfassungTopping[0].classList.remove('topping-0', 'topping-2', 'topping-3');
+        ZusammenfassungTopping[0].classList.add('topping-1');
+        toppingPrice = 0.55;
+    } else if (this.id === 'smarties') {
+        imgtopping.classList.remove('topping0', 'topping1', 'topping3');
+        imgtopping.classList.add('topping2');
+        ZusammenfassungTopping[0].classList.remove('topping-0', 'topping-1', 'topping-3');
+        ZusammenfassungTopping[0].classList.add('topping-2');
+        toppingPrice = 0.45;
+    } else if (this.id === 'gold') {
+        imgtopping.classList.remove('topping0', 'topping1', 'topping2');
+        imgtopping.classList.add('topping3');
+        ZusammenfassungTopping[0].classList.remove('topping-0', 'topping-1', 'topping-2');
+        ZusammenfassungTopping[0].classList.add('topping-3');
+        toppingPrice = 0.65;
+    }
+    toppingPriceField.innerText = toppingPrice.toFixed(2).replace('.',',');
 }
-}
-
 Array.prototype.forEach.call(toppingradios, function (radio) {
 radio.addEventListener('change', changeTopping);
 });
+
+function calculateTotalPrice(event) {
+    totalPrice = 1.00 + creamPrice + stuffingPrice + toppingPrice;
+    totalPriceField.innerText = totalPrice.toFixed(2).replace('.',',');
+}
+document.getElementById("calculate").addEventListener('click', calculateTotalPrice);
